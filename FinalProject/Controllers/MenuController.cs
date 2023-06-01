b132a458c0d9b46d2e58ea8eb9e0c01053824d6c
Thong2705 <thongnltt2705@gmail.com.com>
2023-06-01 21:01:14 +0700

    first commit

---
+++ FinalProject/Controllers/MenuController.cs
@@ -0,0 +1,25 @@
+﻿using FinalProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop.Controllers
+{
+    public class MenuController : Controller
+    {
+        ShopEntities1 _db = new ShopEntities1();
+        // GET: Menu
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult getMenu()
+        {
+            var v = from t in _db.Menus where t.hide == false orderby t.order ascending select t;
+            return PartialView(v);
+        }
+    }
+}
